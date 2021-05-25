277844c435302c4f176df3d5d4fe1e55d354e923
Đỗ Ngọc Trung <ngoctrung@quangich.com>
2021-05-25 21:50:56 +0700

    update tweet header

--- Project/Base/Extension/UIViewExtension.swift
+++ Project/Base/Extension/UIViewExtension.swift
@@ -54,6 +54,13 @@ extension UIView {
         }
     }
     
+    func roundCorners(_ corners:UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
+    
     @IBInspectable var shadowOpacity: Float {
         get {
             return layer.shadowOpacity
@@ -87,6 +94,11 @@ extension UIView {
             }
         }
     }
+    
+    static func nibName() -> String {
+        return String(describing: self.self)
+    }
+    
 }
 
 // MARK: - Programatically constraints
---
+++ Project/Controller/Feeds/FeedsHeaderView/FeedsHeaderCollectionReusableView.swift
@@ -0,0 +1,63 @@
+//
+//  FeedsHeaderCollectionReusableView.swift
+//  Triponus
+//
+//  Created by Be More on 24/05/2021.
+//
+
+import UIKit
+import SDWebImage
+
+protocol FeedsHeaderCollectionReusableViewDelegate: AnyObject {
+    func feedHeaderCollectionView(showProfileOf user: User, view: FeedsHeaderCollectionReusableView)
+    func feedHeaderCollectionView(user: User, tweet view: FeedsHeaderCollectionReusableView)
+    func feedHeaderCollectionView(message view: FeedsHeaderCollectionReusableView)
+}
+
+class FeedsHeaderCollectionReusableView: UICollectionReusableView {
+
+    // MARK: -- Properties
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var tweetView: UIView!
+    
+    weak var delegate: FeedsHeaderCollectionReusableViewDelegate?
+    
+    var user: User? {
+        didSet {
+            self.setUpViews()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        avatarImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleShowProfile(_:))))
+        tweetView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTweet(_:))))
+        DispatchQueue.main.async {
+            self.containerView.roundCorners([.bottomLeft, .bottomRight], radius: 10)
+        }
+    }
+    
+    @objc private func handleShowProfile(_ sender: UITapGestureRecognizer) {
+        guard let user = self.user else { return }
+        self.delegate?.feedHeaderCollectionView(showProfileOf: user, view: self)
+    }
+    
+    @objc private func handleTweet(_ sender: UITapGestureRecognizer) {
+        guard let user = self.user else { return }
+        self.delegate?.feedHeaderCollectionView(user: user, tweet: self)
+    }
+    
+    @IBAction func handleMessage(_ sender: UIButton) {
+        self.delegate?.feedHeaderCollectionView(message: self)
+    }
+}
+
+// MARK: -- Helpers
+extension FeedsHeaderCollectionReusableView {
+    private func setUpViews() {
+        guard let user = self.user else { return }
+        guard let imageUrl = URL(string: user.profileImageUrl) else { return }
+        self.avatarImageView.sd_setImage(with: imageUrl)
+    }
+}
